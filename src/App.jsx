300335056d145920f3bf9f400ec586a1c24c155d
Kenneth Choi <kennethichoi@gmail.com>
2024-05-06 13:04:29 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      App.jsx
+    </div>
+  );
+}
+
+export default App;
